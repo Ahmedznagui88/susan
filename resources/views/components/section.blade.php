2889459b49e01575ba790da9d2cd5375037262da
Ahmed Znagui <amed.bob88@gmail.com>
2024-06-25 22:54:53 +0200

    remove href from id

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,5 +1,5 @@
 
-        <section id="servizi" class="container-fluid section-products">
+        <section class="container-fluid section-products">
             <div class="row">
                 <div class="col-12">
                     <h2 class="title-section">Servizi</h2>
@@ -9,10 +9,10 @@
                 <div data-lag="0.5"  class="col-md-4 div-img">
                     <img  class="first-img img-fluid" src="./media/img/beauty asian girl.avif" alt="">
                 </div>
-                <div class="col-md-4 div-paragraph">
-                    <p data-speed="0.80" class="text-section">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus id nemo itaque sint debitis harum expedita sequi velit quis quae numquam atque culpa asperiores, adipisci dolorem. Dignissimos tenetur ut deserunt?gfhjshdgfjsdhgjshdafkkashjfgbnhdcdhhdvfsd</p>
+                <div data-speed="0.90" class="col-md-4 div-paragraph">
+                    <p  class="text-section">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus id nemo itaque sint debitis harum expedita sequi velit quis quae numquam atque culpa asperiores, adipisci dolorem. Dignissimos tenetur ut deserunt?gfhjshdgfjsdhgjshdafkkashjfgbnhdcdhhdvfsd</p>
                 </div>
-                <div data-lag="1.5"  class="col-md-4 div-img">
+                <div data-lag="0.8"  class="col-md-4 div-img">
                     <img class="first-img img-fluid" src="./media/img/brown girl.avif" alt="">
                 </div>
             </div>
