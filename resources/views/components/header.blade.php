f49c62f91462d63b02e9d7e577a25167f705eb06
Ahmed Znagui <amed.bob88@gmail.com>
2024-06-30 13:21:08 +0200

    add svg

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,12 @@
 <main class="container-fluid container-main">
     <article class="row row-main">
         <figure class="col-md-6 div-img-header">
-        <img class="image-header" src="/media/img/glass.avif" alt=""> 
+        {{-- <img class="image-header" src="/media/img/beauty.avif" alt="">  --}}
         </figure>
 
         <div class="col-md-6 div-header">
             <h1 class="title-header">Exclusive Nails</h1>
-            <small class="text-header">La bellezza che hai sempre voluto</small>
+            <small class="text-header">Eleva il tuo stile con la perfezione delle nostre creazioni per unghie.</small>
             <button class="btn-header">
                 scopri in nostri prodotti
             </button>
