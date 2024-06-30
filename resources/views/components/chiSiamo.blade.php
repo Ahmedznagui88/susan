62e0e2fdeafea842940475a065afebc906f1898e
Ahmed Znagui <amed.bob88@gmail.com>
2024-06-30 22:32:15 +0200

    manto per oggi ho finito

--- resources/views/components/chiSiamo.blade.php
+++ resources/views/components/chiSiamo.blade.php
@@ -1,18 +1,11 @@
 <div class="container-fluid section-product">
-    <div  class="row row-who-we-are">
-        <div class="col-12 wrapper-2">
-            <h2 class="title-section-product">Chi Siamo</h2>
-        </div>
-    </div>
-
-    <div class="container">
         <div class="row row-chiSiamo">
-            <div>
-                <img class="img-fluid img-who-we-re" src="./media/svg/Group 59.png" alt="">
+            <div class="col-md-6" >
+                <img class="img-who-we-re" src="./media/svg/Group 59.png" alt="">
             </div>
-            <div class="col-12 div-paragraph">
-                <p class="text-black">Siamo esperti in onicotecnica, estetica e trucco semipermanente. Offriamo servizi personalizzati per valorizzare la tua bellezza. La nostra passione assicura risultati eccellenti e soddisfazione. Vieni a scoprire il nostro mondo di cura e benessere.</p>
+            <div class="col-md-6 div-paragraph">
+                <h2 class="title-section-product">Chi Siamo</h2>
+                <p class="text-chiSiamo">Esperti in onicotecnica, estetica e trucco semipermanente. Offriamo servizi personalizzati per valorizzare la tua bellezza. La nostra passione assicura risultati eccellenti e soddisfazione. Vieni a scoprire il nostro mondo di cura e benessere.</p>
             </div>
         </div>
-    </div>
 </div>
