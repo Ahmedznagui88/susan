4f1bce6859c5af90553f08c1e881f1c1ba87ddde
Ahmed Znagui <amed.bob88@gmail.com>
2024-07-02 12:20:06 +0200

    almost done eit mm

--- resources/js/imgAnimation.js
+++ resources/js/imgAnimation.js
@@ -1,18 +1,50 @@
-import { gsap } from "gsap";    
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-const smoother = ScrollSmoother.create({
-    smooth:1,
-    effects: true,
-    smoothTouch:0.1,
-});
+function updateSpeed(selector, newSpeed) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+        element.setAttribute('data-speed', newSpeed);
+    });
+}
 
-let jump = document.querySelector(".servizi");
+
+function createSmoother() {
+    return ScrollSmoother.create({
+        smooth: 1,
+        effects: true,
+        smoothTouch: 0.1,
+    });
+}
+
+function handleMediaQueryChange(event) {
+    if (event.matches) {
+        // Se la media query corrisponde, aggiorna il data-speed per dispositivi mobili
+        updateSpeed('.div-img img', 0);
+        updateSpeed('.div-paragraph', 0);
+        updateSpeed('.img-who-we-2', 0);
+        updateSpeed('.img-who-we-re', 0);
+    } else {
+        // Se la media query non corrisponde, usa i valori di default
+        updateSpeed('.div-img img', 0.5);
+        updateSpeed('.div-paragraph', 0.1);
+    }
+
+    if (window.smoother) {
+        window.smoother.kill();
+    }
+    window.smoother = createSmoother();
+}
+
+const mediaQuery = window.matchMedia("(max-width: 900px)");
 
 
-jump.addEventListener("click", (e) => {
-    smoother.scrollTo(".section-products", true, "center 900");
-});
+mediaQuery.addListener(handleMediaQueryChange);
+
+
+handleMediaQueryChange(mediaQuery);
+
+window.smoother = createSmoother();
--- resources/views/components/chiSiamo.blade.php
+++ resources/views/components/chiSiamo.blade.php
@@ -4,11 +4,13 @@
                <h2 class="title-section-product text-light">Siamo, </h2>
                <p class="text-chiSiamo">esperti in onicotecnica, estetica e trucco semipermanente. Offriamo servizi personalizzati per valorizzare la tua bellezza. La nostra passione assicura risultati eccellenti e soddisfazione. Vieni a scoprire il nostro mondo di cura e benessere.</p>
            </div> 
-            <div class="col-md-4 div-inner-img" >
-                <img data-speed="0.2" class="img-who-we-2" src="./media/img/photo-1562048048-86d659689440.avif" alt="">
-            </div>
-            <div  class="col-md-4 div-inner-img" >
-                <img {{-- data-speed="0.6" --}} class="img-who-we-re" src="./media/img/nail.avif" alt="">
-            </div>
+           <div class="col-md-4 div-outer-img">
+               <div class="div-inner-img" >
+                   <img data-lag="0.1" class="img-who-we-2" src="./media/img/photo-1562048048-86d659689440.avif" alt="">
+               </div>
+               <div  class="div-inner-img" >
+                   <img data-lag="0.2" class="img-who-we-re" src="./media/img/nail.avif" alt="">
+               </div>
+           </div>
         </div>
 </div>
