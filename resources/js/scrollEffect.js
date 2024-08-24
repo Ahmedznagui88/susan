511d0746b5e3682ed869f3ffbb8920d8aea8b1f7
Ahmed Znagui <amed.bob88@gmail.com>
2024-08-24 12:34:01 +0200

    convert img to webp

--- resources/js/scrollEffect.js
+++ resources/js/scrollEffect.js
@@ -1,51 +1,44 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-function updateSpeed(selector, newSpeed) {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach((element) => {
-        element.setAttribute("data-speed", newSpeed);
-    });
-}
+const updateSpeed = (selector, newSpeed) => {
+  document.querySelectorAll(selector).forEach(element => {
+    element.setAttribute("data-speed", newSpeed);
+  });
+};
 
-let button = document.querySelector(".contact");
-
-button.addEventListener("click", (e) => {
-    smoother.scrollTo(".adress", true, "bottom 100px");
+const button = document.querySelector(".contact");
+button.addEventListener("click", () => {
+  window.smoother.scrollTo(".adress", true, "bottom 100px");
 });
 
-function createSmoother() {
-    return ScrollSmoother.create({
-        smooth: 0.9,
-        effects: true,
-        smoothTouch: 1,
-    });
-}
+const createSmoother = () => ScrollSmoother.create({
+  smooth: 0.9,
+  effects: true,
+  smoothTouch: 1,
+});
 
-function handleMediaQueryChange(event) {
-    if (event.matches) {
-        updateSpeed(".div-img img", 0);
-        updateSpeed(".paragraph", 0);
-        updateSpeed(".img-who-we-2", 0);
-        updateSpeed(".img-who-we-re", 0);
-    } else {
-        updateSpeed(".div-img img", 0.5);
-        updateSpeed(".paragraph", 0.2);
-    }
+const handleMediaQueryChange = (event) => {
+  if (event.matches) {
+    updateSpeed(".div-img img", 0);
+    updateSpeed(".paragraph", 0);
+    updateSpeed(".img-who-we-2", 0);
+    updateSpeed(".img-who-we-re", 0);
+  } else {
+    updateSpeed(".div-img img", 0.5);
+    updateSpeed(".paragraph", 0.2);
+  }
 
-    if (window.smoother) {
-        window.smoother.kill();
-    }
-    window.smoother = createSmoother();
-}
+  if (window.smoother) {
+    window.smoother.kill();
+  }
+  window.smoother = createSmoother();
+};
 
 const mediaQuery = window.matchMedia("(max-width: 900px)");
-
 mediaQuery.addListener(handleMediaQueryChange);
-
 handleMediaQueryChange(mediaQuery);
 
 window.smoother = createSmoother();
