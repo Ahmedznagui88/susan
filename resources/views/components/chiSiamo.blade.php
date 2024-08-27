f2dde36884256d06f277daebbe4e1910e1c3948e
Ahmed Znagui <amed.bob88@gmail.com>
2024-08-27 18:46:30 +0200

    improve hero section

--- resources/views/components/chiSiamo.blade.php
+++ resources/views/components/chiSiamo.blade.php
@@ -2,7 +2,7 @@
 
     <div class="row row-chiSiamo">
         <div class="col-md-4 div-paragraph">
-            <h2 class="title-section-product">Siamo, </h2>
+            <h2 class="title-section-product">Siamo,</h2>
             <p class="text-chiSiamo">esperti in onicotecnica, estetica e trucco semipermanente. Offriamo servizi
                 personalizzati per valorizzare la tua bellezza. La nostra passione assicura risultati eccellenti e
                 soddisfazione. Vieni a scoprire il nostro mondo di cura e benessere.
@@ -19,10 +19,10 @@
             <div class="div-inner-img">
                 <img data-speed="0.2" class="img-who-we-re" src="./media/img/nail.webp" alt="">
             </div>
-            <div class="div-inner-img">
+{{--             <div class="div-inner-img">
                 <img data-speed="0.1" class="img-who-we-2" src="./media/img/photo-1562048048-86d659689440.webp"
                     alt="">
-            </div>
+            </div> --}}
         </div>
     </div>
 
